374789e3a5ad1ecfd619986528a86e92b12dffa1
m7md4game <46244032+m7md4game@users.noreply.github.com>
2018-12-29 22:40:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,24 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [' v!help','Servers ${client.guilds.size}',' Users ${client.users.size} '];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/m7md4game321`);
+    }, ms);
+    
+});
+
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
