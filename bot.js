c9298d600d3e7e3daea8835d9cf3405b6fd30c73
m7md4game <46244032+m7md4game@users.noreply.github.com>
2018-12-30 20:56:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,6 +100,16 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', async msg => {
+  if(msg.content.startsWith('$servers')) {
+    let output = '**Servers**\n';
+    client.guilds.forEach(guild => {
+      output += `**Name**: ${guild.name}, **ID**: ${guild.id}, **MembersCount**: ${guild.memberCount}, **Owner**: ${guild.owner}`;
+    });
+    msg.channel.send(output);
+  }
+});
+
 const fs = require('fs');
 
 var user = {};
