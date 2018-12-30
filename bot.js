a168a1ca2c86911c494d88fe575b724b431b4786
m7md4game <46244032+m7md4game@users.noreply.github.com>
2018-12-30 16:54:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "$";
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -609,6 +611,75 @@ if (command == "embed") {
 
 
 });
+
+if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+      const reason = message.content.split(" ").slice(1).join(" ");
+      if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`هذا السيرفر ليس لديه \`Support Team\` صنع رتبة, لذلك لن يتم فتح التذكرة.\nاذا كنت تمتلك administrator, إنشاء اسم بهذا الاسم بالضبط وإعطائه للمستخدمين الذين يمكنهم مشاهدة التذاكر.`);
+      if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`انت بالفعل لديك تذكره مفتوحه.`);
+      message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+          let role = message.guild.roles.find("name", "Support Team");
+          let role2 = message.guild.roles.find("name", "@everyone");
+          c.overwritePermissions(role, {
+              SEND_MESSAGES: true,
+              READ_MESSAGES: true
+          });
+          c.overwritePermissions(role2, {
+              SEND_MESSAGES: false,
+              READ_MESSAGES: false
+          });
+          c.overwritePermissions(message.author, {
+              SEND_MESSAGES: true,
+              READ_MESSAGES: true
+          });
+          let mrx = new Discord.RichEmbed()
+          .setColor('RANDOM')
+          .setAuthor(message.author.tag,message.author.avatarURL)
+          .setDescription(`:white_check_mark: تم إنشاء تذكرتك, #${c.name}.`)
+          .setTimestamp()
+          message.channel.sendEmbed(mrx);
+          const embed = new Discord.RichEmbed()
+          .setColor(0xCF40FA)
+          .addField(`مرحباً ${message.author.username}!`, `من فضلك وضح لماذا قمت بفتح التذكره مع بعض التفاصيل. مسؤلي السرفر سوف يكونوا موجودين في اسرع وقت للمساعده.`)
+          .setTimestamp();
+          c.send({ embed: embed });
+      }).catch(console.error);
+  }
+  if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+      if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لا يمكنك استخدام أمر الإغلاق خارج قناة التذاكر.`);
+  
+      message.channel.send(`هل أنت متاكد؟ بمجرد تأكيد, لا يمكنك عكس هذا العمل!!\nللتأكيد ، اكتب \`-confirm\`. سوف ينتهي المهلة خلال 10 ثوانٍ ويتم إلغاؤها.`)
+      .then((m) => {
+        message.channel.awaitMessages(response => response.content === '-confirm', {
+          max: 1,
+          time: 10000,
+          errors: ['time'],
+        })
+        .then((collected) => {
+            message.channel.delete();
+          })
+          .catch(() => {
+            m.edit('انتهى إغلاق التذاكر ، لم يتم إغلاق التذكرة.').then(m2 => {
+                m2.delete();
+            }, 3000);
+          });
+      });
+  }
+  
+  });
+  
+  client.on('message', message => {
+      if (message.content.startsWith(prefix + 'about')) {
+      if (message.author.bot) return
+      if (!message.guild) return message.reply('**:x: This Command Only In Server**')
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(':mailbox_with_mail: about')
+      .setDescription(`I am ${client.user.username}, and I will try my best to help everyone! If I am in a discord server, people can use me to create tickets in order`)
+      .setFooter(`${client.user.username}`)
+      message.author.sendEmbed(embed)
+      }
+  });
+  
 
 client.on('message', message => {
     var args = message.content.split(/[ ]+/)
