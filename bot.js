8fce323d9c9bd083adfec5da3233203d99047e2e
m7md4game <46244032+m7md4game@users.noreply.github.com>
2018-12-30 20:51:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -372,8 +372,9 @@ message.channel.sendEmbed(cat);
 
 client.on('message', async msg => {
   if(msg.content.startsWith('$servers')) {
+    let output = '**Servers**\n';
     client.guilds.forEach(guild => {
-      output = `**Name**: ${guild.name}, **ID**: ${guild.id}, **MembersCount**: ${guild.memberCount}, **Owner**: ${guild.owner}`;
+      output += `**Name**: ${guild.name}, **ID**: ${guild.id}, **MembersCount**: ${guild.memberCount}, **Owner**: ${guild.owner}`;
     });
     msg.channel.send(output);
   }
