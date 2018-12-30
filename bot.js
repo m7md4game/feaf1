0fd6d12ca27827c8b7df600395c9c4bca782b022
m7md4game <46244032+m7md4game@users.noreply.github.com>
2018-12-30 18:09:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -617,8 +617,9 @@ client.on('guildDelete', guild => {
     }
 });
 
+var prefix = "$"
 client.on('message',async message => {
-  if(message.content.startsWith("$setVoice")) {
+  if(message.content.startsWith("prefix + "setVoice")) {
   if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
   if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
   message.channel.send('✅| **تم عمل الروم بنجاح**');
