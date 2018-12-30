f74bb844be337ce7eae4225741da8910c457052f
m7md4game <46244032+m7md4game@users.noreply.github.com>
2018-12-30 20:45:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -609,7 +609,6 @@ client.on('guildDelete', guild => {
 
 client.on('message', async msg => {
   if(msg.content.startsWith('$servers')) {
-    let output = '**Servers**\n';
     client.guilds.forEach(guild => {
       output += `**Name**: ${guild.name}, **ID**: ${guild.id}, **MembersCount**: ${guild.memberCount}, **Owner**: ${guild.owner}`;
     });
