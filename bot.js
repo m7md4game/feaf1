369ecd261a9e8953ad544306bf6f9a0326ddbafd
m7md4game <46244032+m7md4game@users.noreply.github.com>
2018-12-31 06:22:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,32 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`); 
 
 
+client.on("message", message => {
+  let men = message.mentions.users.first();
+  if(message.content.startsWith("$vkick")) {
+    try {
+    if(!men) {
+      message.channel.send("**الرجاء اخيار شخص لطرده !**");
+      return;
+    }
+    if(!message.guild.member(men).voiceChannel) return message.channel.send("المراد طرده ليس في الغرف الصوتيه!");
+    if(!message.member.hasPermission("MOVE_MEMBERS")) return message.channel.send("ليست لديك صلحيات سحب الاعضاء")
+    if(!message.guild.me.hasPermission("MOVE_MEMBERS")) return message.channel.send("ليست لدي الصلاحيه لسحب الاعضاء");
+       if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send("ليست لدي الصلاحيات لانشاء رومات صوتيه")
+
+    message.guild.createChannel(" VKick", "voice").then(c => {
+      message.guild.member(men).setVoiceChannel(c.id)
+    setTimeout(() => {
+      c.delete()
+    }, 100)
+    });
+    message.channel.send(`**لقد تم طرده من الرومات الصوتيه \`\`${men.username}\`\`**`)
+} catch (e) {
+  message.channel.send("لا يمكنني القيام بذلك بسبب الصلاحيات او ما شابه");
+}
+  }
+});	
+	
 client.on("message", msg => {
            var prefix = "$";
   if(msg.content.startsWith (prefix + "id")) {
