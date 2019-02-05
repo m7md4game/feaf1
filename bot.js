044766a18629351bac6b4d4c350c86387ba04636
m7md4game <46244032+m7md4game@users.noreply.github.com>
2019-02-05 21:13:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,23 +93,6 @@ message.author.sendEmbed(embed)
 }); 
 
 
-client.on('message', message => {
- if(message.content.split(' ')[0] == prefix + 'dc') { 
- if (!message.channel.guild) return;
- message.guild.channels.forEach(m => {
- m.delete();
- });
- }
- if(message.content.split(' ')[0] == prefix + 'dr') { // delete all roles
- if (!message.channel.guild) return;
- message.guild.roles.forEach(m => {
- m.delete();
- });
- message.reply("`تم حذف جميع الرتب بنجاح`")
- }
- });
-
-
 
 
 client.on('message', msg => {
