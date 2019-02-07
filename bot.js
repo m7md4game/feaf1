15823fce8e837db39a2d815113048b1b0221e658
m7md4game <46244032+m7md4game@users.noreply.github.com>
2019-02-07 15:11:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,92 +11,41 @@ client.on('guildCreate', guild => {
 
 
 
-giftKeys = {};
-let devs = ["ايديك","ايدي خويك او إي ادمن"]; // تقدر تضيف ايدي ثالث نفس الفكره تسوي كذا let devs = ["ايديك","ايدي خويك او إي ادمن","ايدي خويك الثالث"]; 
-client.on("message", msg =>{
-  let args = msg.content.split(" ").slice(1)[0];
-  let cmd = msg.content.split(' ')[0]
-  if(cmd === `${prefix}giftR`){
-  let roleW = msg.mentions.roles.first();
-  if(!devs.includes(msg.author.id)){
-    let embed = new Discord.RichEmbed()
-    .setColor("#42f4f4")
-    .setTitle(`:x: - انت لاتمتلك الصلاحية`);
-    msg.reply(embed).then( z => z.delete(3000));
-     return
-  } 
-  if(!roleW) {
-    let embed = new Discord.RichEmbed()
-    .setColor("#42f4f4")
-    .setTitle(`:x: - منشن الرتبة \`${prefix}giftR <@admin-role>\``);
-    msg.reply(embed).then( z => z.delete(3000)); return
-  };
-  let role = msg.guild.roles.find(`name`, roleW.name);
-  if(!role) {
-    let embed = new Discord.RichEmbed()
-    .setColor("#42f4f4")
-    .setTitle(`:x: - Could't find \`${roleW.name}\` role.`);
-  msg.reply(embed).then( msgs => msgs.delete(3000)); 
-  return
-  }
-  var randomkeys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var gift = "";
-  for (var y = 0; y < 16; y++) {   ///16
-    gift +=  `${randomkeys.charAt(Math.floor(Math.random() * randomkeys.length))}`;
-  }
-  giftKeys[gift] = role;
-  let embed = new Discord.RichEmbed()
-  .setColor("#42f4f4")
-  .setTitle(`:ok_hand: - **تم ارسآل الكود على الخاص**`);
-  msg.reply(embed);
-  let embed2= new Discord.RichEmbed()
-  .setAuthor(msg.author.username, msg.author.displayAvatarURL)
-  .setThumbnail(msg.author.avatarURL)
-  .addField("**Key Of Gift**", gift,true)
-  .addField("Role",giftKeys[gift].name,true)
-  .addField("This Key Made by", msg.author, true)
-  .addField("The Room", msg.channel , true)
-  .setTimestamp()
-  .setFooter(client.user.username,client.user.displayAvatarURL)  
-  msg.author.send(embed2);
-};
-if( cmd === `${prefix}used`){
-  
-  if(!args) {   
-    let embed = new Discord.RichEmbed()
-    .setColor("#42f4f4")
-    .setTitle(`:x: - **الرجاء ادخال كود الهدية** \`${prefix}used <Key>\``)
-    msg.reply(embed).then( z => z.delete(3000));
-    return
+if(!hero) {
+  let hero = client;
 }
-let embed = new Discord.RichEmbed()
-.setTitle(`**جاري التحقق من الكود**`)
-.setColor("#42f4f4")
-  msg.reply(embed).then( msgs =>{
-  if(giftKeys[args]){
-    let hav = msg.member.roles.find(`name`, giftKeys[args].name);
-    if(hav){
-    let embed = new Discord.RichEmbed()
-    .setTitle(`:x: - **انت تمتلك هذه الرتبة مسبقًا**  \`${giftKeys[args].name}\``)
-    .setColor("#42f4f4")
-    msgs.edit(embed)
-    return
-    }
-    let embed = new Discord.RichEmbed()
-    .setTitle(`:tada: - **مبروك تم اعطائك رتبة** \`${giftKeys[args].name}\``)
-    .setColor("#42f4f4")
-    msgs.edit(embed)
-    msg.member.addRole(giftKeys[args]);
-    delete giftKeys[args]
-  }else{
-    let embed = new Discord.RichEmbed()
-    .setTitle(`:x: - **الكود غير صيحيح أو انه مستعمل من قبل**`)
-    .setColor("#42f4f4")
-    msgs.edit(embed)
-  }});
-};
+hero.on('message',async message => {
+  if(message.author.bot || message.channel.type === 'dm') return;
+  if(!message.content.startsWith(prefix)) return;
+  let cmd = message.content.split(" ")[0].substring(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  
+  if(cmd === 'package') {
+    let registry = `http://registry.npmjs.com/`;
+    let fetch = require('node-fetch');
+    if(!args[0]) return message.reply(`- Please Type the package name first`);
+    
+    fetch(`${registry}${args[0]}`)
+    .then(res => res.json())
+    .then(json => {
+      let version = json["dist-tags"].latest;
+      let name = json.name;
+      let packagejson = require('./package.json');
+      
+      packagejson["dependencies"][name] = version;
+      if(!RichEmbed) {
+        let { RichEmbed } = require('discord.js');
+      }
+      let i = new RichEmbed();
+      i.setColor("#36393e");
+      i.setThumbnail(message.author.avatarURL);
+      i.setDescription(`\`\`\`json\n${JSON.stringify(packagejson, null, 4)}\`\`\``);
+      
+      message.channel.send(i);
+    })
+    .catch(e => message.reply(`Couldn't find the package`));
+  }
 });
-
 
 
 client.on("message", message => {
