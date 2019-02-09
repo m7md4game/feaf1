8c06b56afe538ff6883530350132a0ee656c8658
m7md4game <46244032+m7md4game@users.noreply.github.com>
2019-02-09 17:56:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const coolDown = new Set(); 
+const prefix = "$";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
