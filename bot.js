14544d2e3576c1bda48bf4ec917dfb50712534f2
m7md4game <46244032+m7md4game@users.noreply.github.com>
2019-02-16 20:29:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,16 +6,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  });
-})
 
 var prefix = "$";
 client.on('message',  (message) => {
