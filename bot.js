d1fd5e915c3c74bb9876e66fab86ab7cf273666f
m7md4game <46244032+m7md4game@users.noreply.github.com>
2019-07-10 05:01:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,26 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.content.startsWith("$say")) {
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You dont have** `ADMINISTRATOR` **permission**');
+var args = message.content.trim().split(/ +/g).slice(1);
+let cname = args[0];
+let chan = message.guild.channels.find(element => element.name === cname);
+if (chan) {
+    let text = args.slice(1).join(" ");
+    message.delete();
+    chan.send(text);
+} else {
+    let text = args.join(" ");
+    message.delete();
+    message.channel.send(text);
+   }
+}
+});
+
+
 var prefix = "$"
  
 client.on('message',async message => {
