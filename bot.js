fb2bbd3a440d3277f6835561429bba9d3adc6a22
m7md4game <46244032+m7md4game@users.noreply.github.com>
2019-07-11 02:48:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,40 @@ client.on('message', message => {
   }
 });
 
+const weather = require('weather-js');//npm install weather-js
+client.on('message', message => {
+    let sender = message.author;
+    let cont = message.content.slice(prefix.length).split(" ");
+    let args = cont.slice(1);
+    if (message.content.startsWith('$weather')) {
+
+        weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
+            if (err) message.channel.send(err);
+
+
+            var current = result[0].current;
+            var location = result[0].location;
+
+
+            const embed = new Discord.RichEmbed()
+.setDescription(`**${current.skytext}**`)
+                .setAuthor(`Weather for ${current.observationpoint}`)
+                .setThumbnail(current.imageUrl)
+                .setColor(0x00AE86)
+                .addField('Timezone',`UTC${location.timezone}`, true)
+                .addField('Degree Type',location.degreetype, true)
+                .addField('Temperature',`${current.temperature} Degrees`, true)
+                .addField('Feels Like', `${current.feelslike} Degrees`, true)
+                .addField('Winds',current.winddisplay, true)
+                .addField('Humidity', `${current.humidity}%`, true)
+
+
+                message.channel.send({embed});
+        });
+    }
+
+});
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
