e31ee801fed19401274843a32dbd62428b1f631d
m7md4game <46244032+m7md4game@users.noreply.github.com>
2019-07-11 17:35:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,36 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const coolDown = new Set(); 
+const { getInfoFromName } = require('myanimelists');
+
+          client.on('message', message => {
+            let anime = message.content.split(" ").slice(1).join(" ")
+            if(message.content.startsWith('$anime')) {
+                if(!anime) return message.channel.send('Please Write The Anime Name Example: !anime dragon ball')
+            getInfoFromName(anime)  
+                .then(result => {
+                    let embed = new Discord.RichEmbed()
+                .setTitle(result.title)
+                .addField('Trailer:', `${result.trailer}` || `Unknown`)
+                .addField('Episodes:', `${result.episodes}` || `Unknown`)
+                .addField('Status:', `${result.status}` || `Unknown`)
+                .addField('Studios:', `${result.studios}` || `Unknown`)
+                .addField('Genres:', `${result.genres}` || `Unknown`)
+                .addField('Ranked:', `${result.ranked}` || `Unknown`)
+                .addField('Favorites', `${result.favorites}` || `Unknown`)
+                .addField('Rating:', `${result.rating}` || `Unknown`)
+                .addField('Premiered:', `${result.premiered}` || `Unknown`)
+                .addField('Duration:', `${result.duration}` || `Unknown`)
+                .addField('Score:', `${result.score}` || `Unknown`)
+                .addField('Scored By:', `${result.scoreStats} ` || `Unknown`)
+                .setDescription(`${result.synopsis || 'Unknown'}`)
+                .setImage(result.picture)  
+                .setColor('#0a0000')
+                message.channel.sendEmbed(embed)
+                })
+                .catch(error => console.log(error));
+            }})
+
 
 client.on('message', message => {
   if(message.content.startsWith(`<@${client.user.id}>`)) {
@@ -691,6 +721,7 @@ https://discordapp.com/oauth2/authorize?&client_id=526465331997442048&scope=bot&
 『$rooms / يعرض لك الرومات وعددها』
 『$server / معلومات عن السيرفر』
 『$invites / لرؤية عدد دعواتك في السيرفر』
+『$usersinfo / لعرض معلومات مستخدمين البوت』
 『$animal / يعطيك صور حيوانات』
 『$image / يعرض صوره السيرفر』
 『$roles / يعرض لك جميع الرتب الموجودة بسيرفرك』
