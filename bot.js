a53a77bcf389805baf414d0082d422ac7d4b6220
m7md4game <46244032+m7md4game@users.noreply.github.com>
2019-07-12 21:58:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,25 +3,7 @@ const client = new Discord.Client();
 const coolDown = new Set(); 
 
 
-client.on('message', function(message) {
-        if(message.content.includes('discord.gg')){
-        message.reply (' ')
-           if(!message.channel.guild) return message.reply('** This command only for servers**');
-     message.member.addRole(message.guild.roles.find('name', 'Muted'));
-    const embed500 = new Discord.RichEmbed()
-      .setTitle(":x: | تمت معاقبتك")
-            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
-      .addField(`by`,`ALPHACODES`)
-            .setColor("c91616")
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setAuthor(message.author.username, message.author.avatarURL)
-        .setFooter(`${message.guild.name} Server`)
-     message.channel.send(embed500)
-   
-       
-    }
-    }
-})
+            
 
 client.on('message', message => {
   if(message.content.startsWith(`<@${client.user.id}>`)) {
