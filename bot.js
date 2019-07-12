081992a7d04248d78ab27e96d061f9648db58dcf
m7md4game <46244032+m7md4game@users.noreply.github.com>
2019-07-12 21:32:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,6 @@ const coolDown = new Set();
 
 
 client.on('message', function(message) {
-    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-            let command = message.content.split(" ")[0];
         if(message.content.includes('discord.gg')){
         message.reply (' ')
            if(!message.channel.guild) return message.reply('** This command only for servers**');
